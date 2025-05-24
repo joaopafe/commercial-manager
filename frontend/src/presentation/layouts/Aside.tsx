51e0f566d0fb00e41e6e50d8b8afe79f721ec221
joaopafe <joao.ea2020@gmail.com>
2025-05-24 12:30:16 -0300

    add the Aside component

---
+++ frontend/src/presentation/layouts/Aside.tsx
@@ -0,0 +1,20 @@
+export const Aside = () => {
+  return (
+    <aside className="aside">
+      <div className="company-name">Auto Peças</div>
+
+      <div className="aside-links">
+        <a href="">Início</a>
+        <a href="">Itens</a>
+        <a href="">Estoque</a>
+        <a href="">Fornecedores</a>
+        <a href="">Clientes</a>
+        <a href="">Clientes</a>
+        <a href="">Vendas</a>
+        <a href="">Caixa</a>
+        <a href="">Usuários</a>
+        <a href="">Configurações</a>
+      </div>
+    </aside>
+  );
+};

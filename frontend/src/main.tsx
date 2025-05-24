7a791d51a336b5133cef5ef3b6d006315e85cedb
joaopafe <joao.ea2020@gmail.com>
2025-05-24 12:29:51 -0300

    init the project with vite

---
+++ frontend/src/main.tsx
@@ -0,0 +1,14 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app/App";
+
+import "./presentation/style/reset.css";
+import "./presentation/style/root.css";
+import "./presentation/style/aside.css";
+import "./presentation/style/home.css";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);

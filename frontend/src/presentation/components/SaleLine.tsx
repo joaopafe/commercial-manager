fbde4190469977a2701b85f6fb9a13784785ce76
joaopafe <joao.ea2020@gmail.com>
2025-05-25 00:07:28 -0300

    add the SaleLine component

--- frontend/src/presentation/components/SaleLine.tsx
+++ frontend/src/presentation/components/SaleLine.tsx
@@ -2,20 +2,16 @@ import { FC } from "react";
 
 interface SaleLineProps {
   clientName: string;
-  saleValue: number;
-  saleDate: string;
+  value: number;
+  date: string;
 }
 
-export const SaleLine: FC<SaleLineProps> = ({
-  clientName,
-  saleValue,
-  saleDate,
-}) => {
+export const SaleLine: FC<SaleLineProps> = ({ clientName, value, date }) => {
   return (
     <tr className="sale-line">
       <td className="client-name">{clientName}</td>
-      <td className="sale-value">R$ {saleValue}</td>
-      <td className="sale-date">{saleDate}</td>
+      <td className="sale-value">R$ {value}</td>
+      <td className="sale-date">{date}</td>
     </tr>
   );
 };

e8ad5612a5b519181481b525a83527285e5835a3
joaopafe <joao.ea2020@gmail.com>
2025-05-25 00:10:49 -0300

    add the di on the HomeView

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import { HomeView } from "../../presentation/views/HomeView";
+
+import { HomeViewModelFactory } from "../providers/di/HomeViewModelFactory";
+
+const homeViewModelFactory = new HomeViewModelFactory();
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeView />} />
+        <Route
+          path="/"
+          element={<HomeView homeViewModelFactory={homeViewModelFactory} />}
+        />
       </Routes>
     </BrowserRouter>
   );

0fafbe3fdd6ae8a6667cf2acf68c30bb61bcf180
joaopafe <joao.ea2020@gmail.com>
2025-05-28 22:31:38 -0300

    add the PartsManagerView on index

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -1,10 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { HomeView } from "../../presentation/views/HomeView";
+import { PartsManagerView } from "../../presentation/views/PartsManagerView";
 
 import { HomeViewModelFactory } from "../providers/di/HomeViewModelFactory";
+import { PartsManagerViewModelFactory } from "../providers/di/PartsManagerViewModelFactory";
 
 const homeViewModelFactory = new HomeViewModelFactory();
+const partsManagerViewModelFactory = new PartsManagerViewModelFactory();
 
 export const AppRoutes = () => {
   return (
@@ -13,6 +16,14 @@ export const AppRoutes = () => {
         <Route
           path="/"
           element={<HomeView homeViewModelFactory={homeViewModelFactory} />}
+        />
+        <Route
+          path="/partsmanager"
+          element={
+            <PartsManagerView
+              partsManagerViewModelFactory={partsManagerViewModelFactory}
+            />
+          }
         />
       </Routes>
     </BrowserRouter>

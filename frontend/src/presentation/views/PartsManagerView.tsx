b6369b22e7b7c689e478d20e7877815ee66ce0a5
joaopafe <joao.ea2020@gmail.com>
2025-05-28 22:30:23 -0300

    add the PartsManagerView

---
+++ frontend/src/presentation/views/PartsManagerView.tsx
@@ -0,0 +1,149 @@
+import React, { useEffect, useMemo, useState } from "react";
+
+import { PartsManagerViewModelFactory } from "../../app/providers/di/PartsManagerViewModelFactory";
+
+import { PartsTable } from "../components/PartsTable";
+import { Loader } from "../components/Loader";
+
+interface PartsManagerViewProps {
+  partsManagerViewModelFactory: PartsManagerViewModelFactory;
+}
+
+export const PartsManagerView: React.FC<PartsManagerViewProps> = ({
+  partsManagerViewModelFactory,
+}) => {
+  const partsManagerViewModel = useMemo(
+    () => partsManagerViewModelFactory.makePartsManagerViewModel(),
+    []
+  );
+
+  const [state, setState] = useState(partsManagerViewModel.state);
+
+  useEffect(() => {
+    partsManagerViewModel.stateListener = setState;
+
+    return () => {
+      partsManagerViewModel.stateListener = null;
+    };
+  });
+
+  useEffect(() => {
+    partsManagerViewModel.getParts();
+    partsManagerViewModel.getPieceCategories();
+  }, []);
+
+  const partsTable = state.isSearching ? (
+    <Loader />
+  ) : state.isPartsNotNotFound ? (
+    <div className="information-value">Peças não encontradas</div>
+  ) : (
+    <PartsTable parts={state.parts != null ? state.parts : []} />
+  );
+
+  const categoriesInput = state.isSearching ? (
+    <Loader />
+  ) : state.isPieceCategoriesNotFound ? (
+    <div className="information-value">Categorias não encontradas</div>
+  ) : (
+    <select className="category-input" name="categories" id="category">
+      {state.pieceCategories?.map((pieceCategory, index) => {
+        return (
+          <option key={index} value={pieceCategory.toLocaleLowerCase()}>
+            {pieceCategory}
+          </option>
+        );
+      })}
+    </select>
+  );
+
+  return (
+    <div className="parts-manager">
+      <div className="header">
+        <div className="message">Gerenciamento de Peças</div>
+
+        <button
+          className="new-piece-button"
+          onClick={() => partsManagerViewModel.openModal(true)}
+        >
+          Nova Peça
+        </button>
+      </div>
+
+      {partsTable}
+
+      <div
+        className="piece-modal"
+        style={{ display: state.showCreateModal ? "flex" : "none" }}
+      >
+        <div className="piece-modal-title">Cadastrar Peça</div>
+
+        <form className="piece-modal-form">
+          <label className="name-label" htmlFor="name">
+            Nome:
+          </label>
+          <input className="name-input" type="text" id="name" />
+
+          <label className="category-label" htmlFor="">
+            Categoria
+          </label>
+          {categoriesInput}
+
+          <label className="price-label" htmlFor="price">
+            Preço:
+          </label>
+          <input className="price-input" type="number" id="price" min={0.1} />
+        </form>
+
+        <div className="modal-buttons">
+          <button type="submit" className="confirm-register">
+            Salvar
+          </button>
+          <button
+            type="submit"
+            className="cancel-register"
+            onClick={() => partsManagerViewModel.closeModal()}
+          >
+            Cancelar
+          </button>
+        </div>
+      </div>
+
+      <div
+        className="piece-modal"
+        style={{ display: state.showEditModal ? "flex" : "none" }}
+      >
+        <div className="piece-modal-title">Editar Peça</div>
+
+        <form className="piece-modal-form">
+          <label className="name-label" htmlFor="name">
+            Nome:
+          </label>
+          <input className="name-input" type="text" id="name" />
+
+          <label className="category-label" htmlFor="">
+            Categoria
+          </label>
+          {categoriesInput}
+
+          <label className="price-label" htmlFor="price">
+            Preço:
+          </label>
+          <input className="price-input" type="number" id="price" min={0.1} />
+        </form>
+
+        <div className="modal-buttons">
+          <button type="submit" className="confirm-register">
+            Salvar
+          </button>
+          <button
+            type="submit"
+            className="cancel-register"
+            onClick={() => partsManagerViewModel.closeModal()}
+          >
+            Cancelar
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};

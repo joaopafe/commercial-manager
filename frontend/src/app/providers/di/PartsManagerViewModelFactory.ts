2f38d9b1ffc4c4779bf7db6e1f6bbc3a158026c7
joaopafe <joao.ea2020@gmail.com>
2025-05-30 23:58:16 -0300

    add the supplier dependencies on PartsManager VMFactory

--- frontend/src/app/providers/di/PartsManagerViewModelFactory.ts
+++ frontend/src/app/providers/di/PartsManagerViewModelFactory.ts
@@ -1,27 +1,37 @@
 // Repositories:
 import { PieceRepository } from "../../../domain/data/PieceRepository";
+import { SupplierRepository } from "../../../domain/data/SupplierRepository";
 
 // Data sources:
 import { PieceDataSource } from "../../../domain/data/PieceRepository";
+import { SupplierDataSource } from "../../../domain/data/SupplierRepository";
 
 // Data source implementations:
 import { PieceMockDataSource } from "../../../data/PieceMockDataSource";
+import { SupplierMockDataSource } from "../../../data/SuppilerMockDataSource";
 
 // ViewModel:
 import { PartsManagerViewModel } from "../../../presentation/viewModels/PartsManagerViewModel";
 
 export class PartsManagerViewModelFactory {
   private _pieceDataSource: PieceDataSource;
+  private _supplierDataSource: SupplierDataSource;
 
   private _pieceRepository: PieceRepository;
+  private _supplierRepository: SupplierRepository;
 
   constructor() {
     this._pieceDataSource = new PieceMockDataSource();
+    this._supplierDataSource = new SupplierMockDataSource();
 
     this._pieceRepository = new PieceRepository(this._pieceDataSource);
+    this._supplierRepository = new SupplierRepository(this._supplierDataSource);
   }
 
   makePartsManagerViewModel() {
-    return new PartsManagerViewModel(this._pieceRepository);
+    return new PartsManagerViewModel(
+      this._pieceRepository,
+      this._supplierRepository
+    );
   }
 }

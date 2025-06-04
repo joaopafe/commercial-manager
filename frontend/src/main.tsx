5fbfce9e811db6fec33c542c8208e700bf54cb1d
joaopafe <joao.ea2020@gmail.com>
2025-06-04 20:40:07 -0300

    update the main.tsx with the new styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,9 +5,11 @@ import { App } from "./app/App";
 
 import "./presentation/style/reset.css";
 import "./presentation/style/root.css";
+import "./presentation/style/loader.css";
 import "./presentation/style/aside.css";
 import "./presentation/style/home.css";
 import "./presentation/style/parts-manager.css";
+import "./presentation/style/stock.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

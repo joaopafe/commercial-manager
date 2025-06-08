902179b8e492159f2e400ccf3499d8b45037acfb
joaopafe <joao.ea2020@gmail.com>
2025-06-07 22:39:33 -0300

    update the isPieceValid function

--- frontend/src/domain/validators/isPieceValid.ts
+++ frontend/src/domain/validators/isPieceValid.ts
@@ -1,20 +1,13 @@
 import { Piece } from "../entities/Piece";
 
-export const isPieceValid = (piece: Piece, parts: Piece[]) => {
-  const pieceAlreadyExists =
-    parts.filter((p) => {
-      return p.code === piece.code;
-    }).length === 1;
+export const isPieceValid = (piece: Piece) => {
+  const isPieceValid =
+    piece.name.length >= 3 &&
+    piece.category.length >= 3 &&
+    piece.supplier.length >= 2 &&
+    piece.price >= 0.1;
 
-  if (pieceAlreadyExists) {
-    const isPieceValid =
-      piece.name.length >= 3 &&
-      piece.category.length >= 3 &&
-      piece.supplier.length >= 2 &&
-      piece.price >= 0.1;
-
-    if (isPieceValid) return true;
-  }
+  if (isPieceValid) return true;
 
   return false;
 };

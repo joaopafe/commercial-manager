96217a589e463eba54b3fbf175a1c4728d1b0ee5
joaopafe <joao.ea2020@gmail.com>
2025-06-16 21:50:56 -0300

    update the HomeViewModelFactory

--- frontend/src/app/providers/di/HomeViewModelFactory.ts
+++ frontend/src/app/providers/di/HomeViewModelFactory.ts
@@ -1,15 +1,19 @@
-// Repositories:
-import { SaleRepository } from "../../../domain/data/SaleRepository";
-import { TodaySalesRepository } from "../../../domain/data/TodaySalesRepository";
-import { TotalInCashRepository } from "../../../domain/data/TotalInCashRepository";
+// Use cases:
+import { GetTotalInCash } from "../../../domain/useCases/GetTotalInCash";
+import { GetLatestSales } from "../../../domain/useCases/GetLatestSales";
+import { GetTodaySales } from "../../../domain/useCases/GetTodaySales";
+
+// Repositories implementations:
+import { SaleRepositoryImpl } from "../../../data/repositories/SaleRepositoryImpl";
+import { TotalInCashRepositoryImpl } from "../../../data/repositories/TotalInCashRepositoryImpl";
 
 // Data sources:
-import { SaleDataSource } from "../../../domain/data/SaleRepository";
-import { CashDataSource } from "../../../domain/data/TotalInCashRepository";
+import { SaleDataSource } from "../../../data/repositories/SaleRepositoryImpl";
+import { CashDataSource } from "../../../data/repositories/TotalInCashRepositoryImpl";
 
 // Data source implementations:
-import { SaleMockDataSource } from "../../../data/SaleMockDataSource";
-import { CashMockDataSource } from "../../../data/CashMockDataSource";
+import { SaleMockDataSource } from "../../../data/dataSources/SaleMockDataSource";
+import { CashMockDataSource } from "../../../data/dataSources/CashMockDataSource";
 
 //ViewModel:
 import { HomeViewModel } from "../../../presentation/viewModels/HomeViewModel";
@@ -18,26 +22,30 @@ export class HomeViewModelFactory {
   private _saleDataSource: SaleDataSource;
   private _cashDataSource: CashDataSource;
 
-  private _saleRepository: SaleRepository;
-  private _todaySalesRepository: TodaySalesRepository;
-  private _totalInCashRepository: TotalInCashRepository;
+  private _saleRepository: SaleRepositoryImpl;
+  private _totalInCashRepository: TotalInCashRepositoryImpl;
 
   constructor() {
     this._saleDataSource = new SaleMockDataSource();
     this._cashDataSource = new CashMockDataSource();
 
-    this._todaySalesRepository = new TodaySalesRepository(this._saleDataSource);
-    this._totalInCashRepository = new TotalInCashRepository(
+    this._totalInCashRepository = new TotalInCashRepositoryImpl(
       this._cashDataSource
     );
-    this._saleRepository = new SaleRepository(this._saleDataSource);
+    this._saleRepository = new SaleRepositoryImpl(this._saleDataSource);
   }
 
   makeHomeViewModel() {
+    const getTotalInCashUseCase = new GetTotalInCash(
+      this._totalInCashRepository
+    );
+    const getLatestSalesUseCase = new GetLatestSales(this._saleRepository);
+    const getTodaySalesUseCase = new GetTodaySales(this._saleRepository);
+
     return new HomeViewModel(
-      this._todaySalesRepository,
-      this._totalInCashRepository,
-      this._saleRepository
+      getTotalInCashUseCase,
+      getLatestSalesUseCase,
+      getTodaySalesUseCase
     );
   }
 }

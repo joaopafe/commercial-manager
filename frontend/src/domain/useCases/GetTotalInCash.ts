a867554d4afad4d5fd6819aa9dc34f835cdf4d56
joaopafe <joao.ea2020@gmail.com>
2025-06-16 21:49:07 -0300

    add the GetTotalInCash use case

---
+++ frontend/src/domain/useCases/GetTotalInCash.ts
@@ -0,0 +1,13 @@
+import { TotalInCashRepository } from "../repositories/TotalInCashRepository";
+
+export class GetTotalInCash {
+  constructor(private totalInCashRepository: TotalInCashRepository) {}
+
+  async execute() {
+    const totalInCash = await this.totalInCashRepository.getTotalInCash();
+
+    if (totalInCash === null) Error("Unable to get total cash on hand");
+
+    return totalInCash;
+  }
+}

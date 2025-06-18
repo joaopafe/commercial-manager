457a8cf2b97aca3e680a6fffc0da511d747faebe
joaopafe <joao.ea2020@gmail.com>
2025-06-17 22:05:06 -0300

    remove the method for list the today sales

--- frontend/src/domain/repositories/SaleRepository.ts
+++ frontend/src/domain/repositories/SaleRepository.ts
@@ -1,7 +1,5 @@
 import { Sale } from "../entities/Sale";
-import { TodaySales } from "../entities/TodaySales";
 
 export interface SaleRepository {
   listLatest(): Promise<Sale[] | null>;
-  listTodaySales(): Promise<TodaySales | null>;
 }

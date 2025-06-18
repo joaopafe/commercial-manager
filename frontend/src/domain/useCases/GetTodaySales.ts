7ac9cc2775a244bff3ba288e1576924dd456f405
joaopafe <joao.ea2020@gmail.com>
2025-06-17 22:05:43 -0300

    add the GetTodaySales use case

--- frontend/src/domain/useCases/GetTodaySales.ts
+++ frontend/src/domain/useCases/GetTodaySales.ts
@@ -1,13 +1,25 @@
+import { TodaySales } from "../entities/TodaySales";
+
 import { SaleRepository } from "../repositories/SaleRepository";
 
 export class GetTodaySales {
   constructor(private saleRepository: SaleRepository) {}
 
-  async execute() {
-    const todaySales = await this.saleRepository.listTodaySales();
+  async execute(): Promise<TodaySales | Error> {
+    const latestSales = await this.saleRepository.listLatest();
 
-    if (todaySales === null) Error("Unable to get today's sales");
+    if (latestSales && latestSales.length > 0) {
+      let todaySales = 0;
 
-    return todaySales;
+      for (const sale of latestSales) {
+        todaySales += sale.value;
+      }
+
+      return todaySales;
+    }
+
+    if (latestSales === null) Error("Unable to get today's sales");
+
+    return 0;
   }
 }

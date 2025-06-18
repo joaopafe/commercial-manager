1c92e36c139f6bfcb52150295f4adba6dd442eab
joaopafe <joao.ea2020@gmail.com>
2025-06-17 21:58:03 -0300

    update the PartsManagerViewModel

--- frontend/src/app/providers/di/PartsManagerViewModelFactory.ts
+++ frontend/src/app/providers/di/PartsManagerViewModelFactory.ts
@@ -1,14 +1,22 @@
-// Repositories:
-import { PieceRepository } from "../../../domain/data/PieceRepository";
-import { SupplierRepository } from "../../../domain/data/SupplierRepository";
+// Use cases:
+import { GetParts } from "../../../domain/useCases/GetParts";
+import { GetPieceCategories } from "../../../domain/useCases/GetPieceCategories";
+import { CreatePiece } from "../../../domain/useCases/CreatePiece";
+import { EditPiece } from "../../../domain/useCases/EditPiece";
+import { RemovePiece } from "../../../domain/useCases/RemovePiece";
+import { GetSuppliers } from "../../../domain/useCases/GetSuppliers";
+
+// Repositories implementations:
+import { PieceRepositoryImpl } from "../../../data/repositories/PieceRepositoryImpl";
+import { SupplierRepositoryImpl } from "../../../data/repositories/SupplierRepositoryImpl";
 
 // Data sources:
-import { PieceDataSource } from "../../../domain/data/PieceRepository";
-import { SupplierDataSource } from "../../../domain/data/SupplierRepository";
+import { PieceDataSource } from "../../../data/repositories/PieceRepositoryImpl";
+import { SupplierDataSource } from "../../../data/repositories/SupplierRepositoryImpl";
 
 // Data source implementations:
-import { PieceMockDataSource } from "../../../data/PieceMockDataSource";
-import { SupplierMockDataSource } from "../../../data/SuppilerMockDataSource";
+import { PieceMockDataSource } from "../../../data/dataSources/PieceMockDataSource";
+import { SupplierMockDataSource } from "../../../data/dataSources/SuppilerMockDataSource";
 
 // ViewModel:
 import { PartsManagerViewModel } from "../../../presentation/viewModels/PartsManagerViewModel";
@@ -17,21 +25,34 @@ export class PartsManagerViewModelFactory {
   private _pieceDataSource: PieceDataSource;
   private _supplierDataSource: SupplierDataSource;
 
-  private _pieceRepository: PieceRepository;
-  private _supplierRepository: SupplierRepository;
+  private _pieceRepository: PieceRepositoryImpl;
+  private _supplierRepository: SupplierRepositoryImpl;
 
   constructor() {
     this._pieceDataSource = new PieceMockDataSource();
     this._supplierDataSource = new SupplierMockDataSource();
 
-    this._pieceRepository = new PieceRepository(this._pieceDataSource);
-    this._supplierRepository = new SupplierRepository(this._supplierDataSource);
+    this._pieceRepository = new PieceRepositoryImpl(this._pieceDataSource);
+    this._supplierRepository = new SupplierRepositoryImpl(
+      this._supplierDataSource
+    );
   }
 
   makePartsManagerViewModel() {
+    const getParts = new GetParts(this._pieceRepository);
+    const getPieceCategories = new GetPieceCategories(this._pieceRepository);
+    const createPiece = new CreatePiece(this._pieceRepository);
+    const editPiece = new EditPiece(this._pieceRepository);
+    const removePiece = new RemovePiece(this._pieceRepository);
+    const getSuppliers = new GetSuppliers(this._supplierRepository);
+
     return new PartsManagerViewModel(
-      this._pieceRepository,
-      this._supplierRepository
+      getParts,
+      getPieceCategories,
+      createPiece,
+      editPiece,
+      removePiece,
+      getSuppliers
     );
   }
 }

cdaa0aac3a61a773b3e37f18cf531c17bb072d92
joaopafe <joao.ea2020@gmail.com>
2025-06-28 12:31:05 -0300

    add the insertStock method on StockRepository

--- frontend/src/domain/repositories/StockRepository.ts
+++ frontend/src/domain/repositories/StockRepository.ts
@@ -2,4 +2,12 @@ import { StockGroup } from "../entities/StockGroup";
 
 export interface StockRepository {
   list(): Promise<StockGroup[] | null>;
+  insertStock(
+    pieceCode: number,
+    quantity: number
+  ): Promise<StockGroup[] | Error>;
+  removeStock(
+    pieceCode: number,
+    quantity: number
+  ): Promise<StockGroup[] | Error>;
 }

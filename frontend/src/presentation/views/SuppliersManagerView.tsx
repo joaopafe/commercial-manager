71c535ba97eafc655f3e943db8d63cf6c1ac5af0
joaopafe <joao.ea2020@gmail.com>
2025-07-05 12:31:34 -0300

    add the SuppliersManagerVM integration with SuppliersManagerView

--- frontend/src/presentation/views/SuppliersManagerView.tsx
+++ frontend/src/presentation/views/SuppliersManagerView.tsx
@@ -1,6 +1,46 @@
-import { SuppliersTable } from "../components/SuppliersTable";
+import React, { useEffect, useMemo, useState } from "react";
 
-export const SuppliersManagerView = () => {
+import { SuppliersManagerViewModelFactory } from "../../app/providers/di/SuppliersManagerViewModelFactory";
+
+import { SuppliersTable } from "../components/SuppliersTable";
+import { Loader } from "../components/Loader";
+
+interface SuppliersManagerViewProps {
+  supplierManagerViewModelFactory: SuppliersManagerViewModelFactory;
+}
+
+export const SuppliersManagerView: React.FC<SuppliersManagerViewProps> = ({
+  supplierManagerViewModelFactory,
+}) => {
+  const suppliersManagerViewModel = useMemo(
+    () => supplierManagerViewModelFactory.makeSuppliersManagerViewModel(),
+    []
+  );
+
+  const [state, setState] = useState(suppliersManagerViewModel.state);
+
+  useEffect(() => {
+    suppliersManagerViewModel.stateListener = setState;
+
+    return () => {
+      suppliersManagerViewModel.stateListener = null;
+    };
+  });
+
+  useEffect(() => {
+    suppliersManagerViewModel.getSuppliers();
+  }, []);
+
+  const suppliersTable = state.isSearching ? (
+    <Loader />
+  ) : state.isSuppliersNotFound ? (
+    <div className="information-value">Fornecedores não encontrados</div>
+  ) : (
+    <SuppliersTable
+      suppliers={state.suppliers != null ? state.suppliers : []}
+    />
+  );
+
   return (
     <div className="suppliers-manager">
       <div className="header">
@@ -8,34 +48,8 @@ export const SuppliersManagerView = () => {
 
         <button className="new-supplier-button">Novo Fornecedor</button>
       </div>
-      <SuppliersTable
-        suppliers={[
-          {
-            code: 1,
-            cnpj: "12.345.678/0001-90",
-            name: "Auto Partes LTDA",
-            phone: "(15) 99231-2001",
-          },
-          {
-            code: 2,
-            cnpj: "16.456.965/0001-77",
-            name: "Freios Brasil",
-            phone: "(11) 3324-2132",
-          },
-          {
-            code: 3,
-            cnpj: "11.314.556/0001-43",
-            name: "GM",
-            phone: "(19) 4321-4344",
-          },
-          {
-            code: 4,
-            cnpj: "10.617.432/0001-31",
-            name: "Bosh",
-            phone: "(11) 3232-3910",
-          },
-        ]}
-      />
+
+      {suppliersTable}
 
       <div className="supplier-modal" style={{ display: "none" }}>
         <div className="supplier-modal-title">Cadastrar Fornecedor</div>

0b1d41209c87f417073b6a3575b22f506d421c8a
joaopafe <joao.ea2020@gmail.com>
2025-07-05 12:29:39 -0300

    update the SupplierMockDataSource with the new Supplier

--- frontend/src/data/dataSources/SuppilerMockDataSource.ts
+++ frontend/src/data/dataSources/SuppilerMockDataSource.ts
@@ -5,14 +5,54 @@ import { SupplierDataSource } from "../repositories/SupplierRepositoryImpl";
 import { delay } from "../../shared/utils/delay";
 
 const suppliersMock: Supplier[] = [
-  "Visconde",
-  "GM",
-  "Bosh",
-  "Mobil",
-  "Valeo",
-  "Delphi",
-  "Magneti Marelli",
-  "Notus",
+  {
+    code: 1,
+    cnpj: "51.955.546/0001-37",
+    name: "Visconde",
+    phone: "(85) 98568-8035",
+  },
+  {
+    code: 2,
+    cnpj: "76.184.908/0001-64",
+    name: "GM",
+    phone: "(98) 97030-5884",
+  },
+  {
+    code: 3,
+    cnpj: "84.083.564/0001-25",
+    name: "Bosch",
+    phone: "(84) 96465-1846",
+  },
+  {
+    code: 4,
+    cnpj: "09.339.556/0001-81",
+    name: "Mobil",
+    phone: "(69) 97806-2726",
+  },
+  {
+    code: 5,
+    cnpj: "51.377.809/0001-78",
+    name: "Valeo",
+    phone: "(64) 96527-2326",
+  },
+  {
+    code: 6,
+    cnpj: "99.203.973/0001-95",
+    name: "Delphi",
+    phone: "(46) 96621-0540",
+  },
+  {
+    code: 7,
+    cnpj: "28.258.156/0001-96",
+    name: "Magneti Marelli",
+    phone: "(63) 97604-6784",
+  },
+  {
+    code: 8,
+    cnpj: "20.614.041/0001-85",
+    name: "Notus",
+    phone: "(45) 96300-6731",
+  },
 ];
 
 export class SupplierMockDataSource implements SupplierDataSource {

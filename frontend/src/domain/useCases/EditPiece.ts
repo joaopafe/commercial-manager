c96d377b79f10968d36085617c2743a2a63d022e
joaopafe <joao.ea2020@gmail.com>
2025-07-06 23:03:16 -0300

    update the EditPiece use case

--- frontend/src/domain/useCases/EditPiece.ts
+++ frontend/src/domain/useCases/EditPiece.ts
@@ -1,32 +1,51 @@
 import { Piece } from "../entities/Piece";
 
 import { PieceRepository } from "../repositories/PieceRepository";
+import { SupplierRepository } from "../repositories/SupplierRepository";
 
-import { pieceAlreadyExists } from "../validators/pieceAlreadyExists";
 import { isPieceValid as pieceValid } from "../validators/isPieceValid";
+import { pieceAlreadyExists } from "../validators/pieceAlreadyExists";
+import { supplierAlreadyExists } from "../validators/supplierAlreadyExists";
+import { categoryAlreadyExists } from "../validators/categoryAlreadyExists";
 
 export class EditPiece {
-  constructor(private pieceRepository: PieceRepository) {}
+  constructor(
+    private pieceRepository: PieceRepository,
+    private supplierRepository: SupplierRepository
+  ) {}
 
   async exec(piece: Piece): Promise<Piece | Error> {
+    const isPieceValid = pieceValid(piece);
+    if (!isPieceValid) return Error("The piece is invalid");
+
     const parts = await this.pieceRepository.listPieces();
+    const suppliers = await this.supplierRepository.list();
+    const pieceCategories = await this.pieceRepository.listCategories();
 
-    if (parts) {
+    if (parts && suppliers && pieceCategories) {
       const pieceExists = pieceAlreadyExists(piece.code, parts);
+      const supplierExists = supplierAlreadyExists(
+        piece.supplierCode,
+        suppliers
+      );
+      const pieceCategoryExists = categoryAlreadyExists(
+        piece.categoryCode,
+        pieceCategories
+      );
 
-      if (pieceExists) {
-        const isPieceValid = pieceValid(piece);
+      if (pieceExists && supplierExists && pieceCategoryExists) {
+        const editedPiece = await this.pieceRepository.editPiece(piece);
 
-        if (isPieceValid) {
-          const editedPiece = await this.pieceRepository.editPiece(piece);
-
-          return editedPiece;
-        }
+        return editedPiece;
       }
 
-      return Error("The parts is invalid for editing");
+      return pieceExists
+        ? supplierExists
+          ? Error("The piece category code is invalid")
+          : Error("The supplier code is invalid")
+        : Error("The piece code is invalid");
     }
 
-    return Error("It was not possible to edit the piece");
+    return Error("It was not possible to edit piece");
   }
 }

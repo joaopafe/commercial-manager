20947342251b7ea8b930a8be0a27c2a4a4465a69
joaopafe <joao.ea2020@gmail.com>
2025-07-06 23:07:24 -0300

    udpate the StockTable component

--- frontend/src/presentation/components/StockTable.tsx
+++ frontend/src/presentation/components/StockTable.tsx
@@ -2,10 +2,12 @@ import { FC } from "react";
 
 import { StockLine } from "./StockLine";
 
-import { StockProps } from "./StockLine";
+import { PieceStock } from "../../domain/entities/PieceStock";
+import { Supplier } from "../../domain/entities/Supplier";
 
 interface StockTableProps {
-  items: StockProps[];
+  items: PieceStock[];
+  suppliers: Supplier[];
   openModal(isEntryStockModal: boolean): void;
   changePieceCode(pieceCode: number): void;
   changeTotalQuantity(totalQuantity: number): void;
@@ -13,16 +15,21 @@ interface StockTableProps {
 
 export const StockTable: FC<StockTableProps> = ({
   items,
+  suppliers,
   openModal,
   changePieceCode,
   changeTotalQuantity,
 }) => {
   const stockLines = items.map((item) => {
+    const supplier = suppliers.find(
+      (supplier) => supplier.code === item.supplierCode
+    );
+
     return (
       <StockLine
         code={item.code}
         name={item.name}
-        supplier={item.supplier}
+        supplier={supplier ? supplier.name : ""}
         quantity={item.quantity}
         price={item.price}
         key={item.code}

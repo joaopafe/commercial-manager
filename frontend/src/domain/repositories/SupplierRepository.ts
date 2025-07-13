f79bd71ecbf9a4cd69dd51fbb461db4e197a6664
joaopafe <joao.ea2020@gmail.com>
2025-07-12 22:16:57 -0300

    add the edit method on SupplierRepository

--- frontend/src/domain/repositories/SupplierRepository.ts
+++ frontend/src/domain/repositories/SupplierRepository.ts
@@ -5,4 +5,5 @@ import { AddSupplierParams } from "../useCases/CreateSupplier";
 export interface SupplierRepository {
   list(): Promise<Supplier[] | null>;
   create(supplier: AddSupplierParams): Promise<Supplier | Error>;
+  edit(supplier: Supplier): Promise<Supplier | Error>;
 }

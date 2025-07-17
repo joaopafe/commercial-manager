201d6097e91a8cf94b436ccb20eec6f8e12c8544
joaopafe <joao.ea2020@gmail.com>
2025-07-17 11:14:40 -0300

    add the customers-manager.css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,12 +6,13 @@ import { App } from "./app/App";
 import "./presentation/style/reset.css";
 import "./presentation/style/root.css";
 import "./presentation/style/loader.css";
+import "./presentation/style/toast.css";
 import "./presentation/style/aside.css";
 import "./presentation/style/home.css";
 import "./presentation/style/parts-manager.css";
 import "./presentation/style/stock.css";
 import "./presentation/style/suppliers-manager.css";
-import "./presentation/style/toast.css";
+import "./presentation/style/customers-manager.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

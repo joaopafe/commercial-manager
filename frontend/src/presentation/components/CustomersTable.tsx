f8b3f945eb7ce6b9f66c11ec7b20ad92c95b0a8f
joaopafe <joao.ea2020@gmail.com>
2025-07-17 11:13:31 -0300

    add the CustomerTable component

---
+++ frontend/src/presentation/components/CustomersTable.tsx
@@ -0,0 +1,58 @@
+import { FC } from "react";
+
+import { CustomerLine } from "./CustomerLine";
+
+import { CustomerProps as Customer } from "./CustomerLine";
+
+interface CustomerTableProps {
+  customers: Customer[];
+  openModal(isCreateModal: boolean): void;
+}
+
+export const CustomersTable: FC<CustomerTableProps> = ({
+  customers,
+  openModal,
+}) => {
+  const customerLines = customers.map((customer) => {
+    return (
+      <CustomerLine
+        code={customer.code}
+        cpf={customer.cpf}
+        name={customer.name}
+        email={customer.email}
+        phone={customer.phone}
+        key={customer.code}
+        openModal={openModal}
+      />
+    );
+  });
+
+  return (
+    <table className="customers-table">
+      <thead className="customers-table-head">
+        <tr>
+          <th className="customers-table-head-column" id="customer-code-head">
+            Código
+          </th>
+          <th className="customers-table-head-column" id="customer-cpf-head">
+            CPF
+          </th>
+          <th className="customers-table-head-column" id="customer-name-head">
+            Nome
+          </th>
+          <th className="customers-table-head-column" id="customer-email-head">
+            E-mail
+          </th>
+          <th className="customers-table-head-column" id="customer-phone-head">
+            Telefone
+          </th>
+          <th className="customers-table-head-column" id="customer-action-head">
+            Ações
+          </th>
+        </tr>
+      </thead>
+
+      <tbody className="customers-table-body">{customerLines}</tbody>
+    </table>
+  );
+};

9c3dd518dfc78a946b0375214d9a781de7757ace
joaopafe <joao.ea2020@gmail.com>
2025-07-17 11:14:15 -0300

    add the CustomersManagerVMFactory impl

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -4,16 +4,19 @@ import { HomeView } from "../../presentation/views/HomeView";
 import { PartsManagerView } from "../../presentation/views/PartsManagerView";
 import { StockView } from "../../presentation/views/StockView";
 import { SuppliersManagerView } from "../../presentation/views/SuppliersManagerView";
+import { CustomersManagerView } from "../../presentation/views/CustomersManagerView";
 
 import { HomeViewModelFactory } from "../providers/di/HomeViewModelFactory";
 import { PartsManagerViewModelFactory } from "../providers/di/PartsManagerViewModelFactory";
 import { StockViewModelFactory } from "../providers/di/StockViewModelFactory";
 import { SuppliersManagerViewModelFactory } from "../providers/di/SuppliersManagerViewModelFactory";
+import { CustomersManagerViewModelFactory } from "../providers/di/CustomersManagerViewModelFactory";
 
 const homeViewModelFactory = new HomeViewModelFactory();
 const partsManagerViewModelFactory = new PartsManagerViewModelFactory();
 const stockViewModelFactory = new StockViewModelFactory();
 const suppliersManagerViewModelFactory = new SuppliersManagerViewModelFactory();
+const customersManagerViewModelFactory = new CustomersManagerViewModelFactory();
 
 export const AppRoutes = () => {
   return (
@@ -39,6 +42,14 @@ export const AppRoutes = () => {
         element={
           <SuppliersManagerView
             supplierManagerViewModelFactory={suppliersManagerViewModelFactory}
+          />
+        }
+      />
+      <Route
+        path="customersmanager"
+        element={
+          <CustomersManagerView
+            customerManagerViewModelFactory={customersManagerViewModelFactory}
           />
         }
       />

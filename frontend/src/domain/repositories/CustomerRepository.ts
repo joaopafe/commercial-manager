f1c37934d0e2e17203cc3befaadeca8b54dd6b1a
joaopafe <joao.ea2020@gmail.com>
2025-07-18 16:36:28 -0300

    add the create method os CustomerRepository

--- frontend/src/domain/repositories/CustomerRepository.ts
+++ frontend/src/domain/repositories/CustomerRepository.ts
@@ -1,5 +1,8 @@
 import { Customer } from "../entities/Customer";
 
+import { AddCustomerParams } from "../useCases/CreateCustomer";
+
 export interface CustomerRepository {
   list(): Promise<Customer[] | null>;
+  create(customer: AddCustomerParams): Promise<Customer | Error>;
 }

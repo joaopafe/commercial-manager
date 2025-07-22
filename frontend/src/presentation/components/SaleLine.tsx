01090cb15525a54084dbaa4d2c2cf921bda8482b
joaopafe <joao.ea2020@gmail.com>
2025-07-22 15:00:51 -0300

    update the SaleLine component

--- frontend/src/presentation/components/SaleLine.tsx
+++ frontend/src/presentation/components/SaleLine.tsx
@@ -2,14 +2,21 @@ import { FC } from "react";
 
 interface SaleLineProps {
   clientName: string;
+  description: string;
   value: number;
   date: string;
 }
 
-export const SaleLine: FC<SaleLineProps> = ({ clientName, value, date }) => {
+export const SaleLine: FC<SaleLineProps> = ({
+  clientName,
+  description,
+  value,
+  date,
+}) => {
   return (
     <tr className="sale-line">
       <td className="client-name">{clientName}</td>
+      <td className="sale-description">{description}</td>
       <td className="sale-value">R$ {value}</td>
       <td className="sale-date">{date}</td>
     </tr>

9f75475722d0067162eec1d904f2511f69ecbcf0
joaopafe <joao.ea2020@gmail.com>
2025-07-23 20:55:43 -0300

    add the productPurchaseMapper

---
+++ frontend/src/domain/mappers/productPurchaseMapper.ts
@@ -0,0 +1,29 @@
+import { ProductPurchase } from "../entities/ProductPurchase";
+import { Supplier } from "../entities/Supplier";
+import { Piece } from "../entities/Piece";
+import { GeneralPurchase } from "../entities/GeneralPurchase";
+
+export const productPurchaseMapper = (
+  productPurchases: ProductPurchase[],
+  suppliers: Supplier[],
+  parts: Piece[]
+): GeneralPurchase[] => {
+  const mappedProductPurchases = productPurchases.map((productPurchase) => {
+    const supplier = suppliers.find((supplier) => {
+      return supplier.code === productPurchase.supplierId;
+    });
+
+    const piece = parts.find((piece) => {
+      return piece.code === productPurchase.pieceId;
+    });
+
+    return {
+      supplierName: supplier ? supplier.name : "",
+      name: piece ? `${productPurchase.quantity}X ${piece.name}` : "",
+      value: productPurchase.value,
+      date: productPurchase.date,
+    };
+  });
+
+  return mappedProductPurchases;
+};

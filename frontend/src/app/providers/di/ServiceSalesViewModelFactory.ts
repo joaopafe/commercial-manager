7ff25840597a666af26a8d1b62361d6e18575391
joaopafe <joao.ea2020@gmail.com>
2025-07-25 11:48:13 -0300

    update the ServiceSalesViewModeLFactory

---
+++ frontend/src/app/providers/di/ServiceSalesViewModelFactory.ts
@@ -0,0 +1,43 @@
+// Use cases:
+import { GetServiceSales } from "../../../domain/useCases/GetServiceSales";
+import { GetCustomers } from "../../../domain/useCases/GetCustomers";
+
+// Repositories implementations:
+import { SaleRepositoryImpl } from "../../../data/repositories/SaleRepositoryImpl";
+import { CustomerRepositoryImpl } from "../../../data/repositories/CustomerRepositoryImpl";
+
+// Data sources:
+import { SaleDataSource } from "../../../data/repositories/SaleRepositoryImpl";
+import { CustomerDataSource } from "../../../data/repositories/CustomerRepositoryImpl";
+
+// Data source implementations:
+import { SaleMockDataSource } from "../../../data/dataSources/SaleMockDataSource";
+import { CustomerMockDataSource } from "../../../data/dataSources/CustomersMockDataSource";
+
+// View model:
+import { ServiceSalesViewModel } from "../../../presentation/viewModels/ServiceSalesViewMode";
+
+export class ServiceSalesViewModelFactory {
+  private _saleDataSource: SaleDataSource;
+  private _customerDataSource: CustomerDataSource;
+
+  private _saleRepository: SaleRepositoryImpl;
+  private _customerRepository: CustomerRepositoryImpl;
+
+  constructor() {
+    this._saleDataSource = new SaleMockDataSource();
+    this._customerDataSource = new CustomerMockDataSource();
+
+    this._saleRepository = new SaleRepositoryImpl(this._saleDataSource);
+    this._customerRepository = new CustomerRepositoryImpl(
+      this._customerDataSource
+    );
+  }
+
+  makeServiceSalesViewModel() {
+    const getServiceSales = new GetServiceSales(this._saleRepository);
+    const getCustomer = new GetCustomers(this._customerRepository);
+
+    return new ServiceSalesViewModel(getServiceSales, getCustomer);
+  }
+}

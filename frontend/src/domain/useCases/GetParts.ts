7300623d0f4545c8bee98aa4c4528204678efd8d
joaopafe <joao.ea2020@gmail.com>
2025-07-28 16:00:51 -0300

    refac the name of method on GetParts use case

--- frontend/src/domain/useCases/GetParts.ts
+++ frontend/src/domain/useCases/GetParts.ts
@@ -3,7 +3,7 @@ import { PieceRepository } from "../repositories/PieceRepository";
 export class GetParts {
   constructor(private pieceRepository: PieceRepository) {}
 
-  async execute() {
+  async exec() {
     const parts = await this.pieceRepository.listPieces();
 
     if (parts === null) Error("Unable to get parts");

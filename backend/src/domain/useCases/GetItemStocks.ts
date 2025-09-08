93290d05950cb81f8f8ec363218fc67c11ce326f
joaopafe <joao.ea2020@gmail.com>
2025-09-08 12:00:55 -0300

    add the GetItemStocks use case

---
+++ backend/src/domain/useCases/GetItemStocks.ts
@@ -0,0 +1,11 @@
+import { ItemRepository } from "../repositories/ItemRepository";
+
+export class GetItemStocks {
+  constructor(private itemRepository: ItemRepository) {}
+
+  async exec() {
+    const itemStocks = await this.itemRepository.getAllStocks();
+
+    return itemStocks;
+  }
+}

acbdbb9ed6eeaeb07749c94362e39faf097f5f46
joaopafe <joao.ea2020@gmail.com>
2025-09-09 22:26:30 -0300

    update the ParamsForSupplierCreation interface with encapsulated types

--- backend/src/domain/repositories/SupplierRepository.ts
+++ backend/src/domain/repositories/SupplierRepository.ts
@@ -1,15 +1,16 @@
-import { Id, CNPJ, Name, Phone, Supplier } from "../entities/Supplier";
+import { CNPJ, Name, Phone, Supplier } from "../entities/Supplier";
+import { Id } from "../entities/shared/Id";
 
-export interface AddSupplierParams {
-  cnpj: string;
-  name: string;
-  phone: string;
+export interface ParamsForSupplierCreation {
+  cnpj: CNPJ;
+  name: Name;
+  phone: Phone;
 }
 
 export interface SupplierRepository {
   getAllSuppliers(): Promise<Supplier[] | null>;
   getSupplierById(id: Id): Promise<Supplier | null>;
-  createSupplier(supplier: AddSupplierParams): Promise<Supplier>;
+  createSupplier(supplier: ParamsForSupplierCreation): Promise<Supplier>;
   updateSupplier(supplier: Supplier): Promise<Supplier>;
   removeSupplier(id: Id): Promise<Supplier>;
 }

5a718926023bda4170d03ed695d2de863e23a54c
joaopafe <joao.ea2020@gmail.com>
2025-09-13 12:33:03 -0300

    update the configDB with the pool function

--- backend/configDB.ts
+++ backend/configDB.ts
@@ -1,30 +1,12 @@
-import { Client } from "pg";
-
+import { Pool } from "pg";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const client = new Client({
+export const pool = new Pool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 });
-
-async function connectAndQuery() {
-  try {
-    await client.connect();
-    console.log("Connecting to database....");
-
-    // Query example
-    const res = await client.query("SELECT NOW()");
-    console.log("Current time:", res.rows[0]);
-  } catch (err) {
-    console.error("Error connecting to database", err);
-  } finally {
-    await client.end();
-  }
-}
-
-connectAndQuery();

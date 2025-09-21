3c25007bfe50eb964fd8a26a9005e6f4544078bb
joaopafe <joao.ea2020@gmail.com>
2025-09-21 10:04:46 -0300

    add the ProductSaleDataSource

---
+++ backend/src/data/dataSources/ProductSaleDataSource.ts
@@ -0,0 +1,189 @@
+import { QueryResult } from "pg";
+
+import { pool } from "../../../configDB";
+
+import { DomainError } from "../../domain/entities/errors/DomainError";
+import { ProductSaleError } from "../../domain/entities/errors/ProductSaleError";
+
+export interface ProductSale {
+  id: number;
+  customerId: number;
+  itemId: number;
+  quantity: number;
+  value: number;
+  date: Date;
+}
+
+export class ProductSaleDataSource {
+  static async createTable() {
+    const query = `
+            CREATE TABLE IF NOT EXISTS product_sales
+            (
+                id SERIAL PRIMARY KEY,
+                customer_id INT NOT NULL,
+                item_id INT NOT NULL,
+                quantity INT NOT NULL,
+                value FLOAT NOT NULL,
+                date DATE NOT NULL
+            );
+        `;
+
+    try {
+      await pool.query(query);
+
+      return true;
+    } catch (error) {
+      throw new DomainError(
+        "unknown",
+        `It was not possible to create the product sale table: ${error}`
+      );
+    }
+  }
+
+  static async findAll(): Promise<ProductSale[]> {
+    const query = `
+        SELECT * FROM product_sales;
+    `;
+
+    try {
+      const productSales: QueryResult<ProductSale> = await pool.query(query);
+
+      return productSales.rows;
+    } catch (error) {
+      throw new DomainError(
+        "unknown",
+        `It was not possible to get the product sales: ${error}`
+      );
+    }
+  }
+
+  static async findById(id: number): Promise<ProductSale> {
+    const query = `
+        SELECT * FROM product_sales
+        WHERE id = $1
+    `;
+
+    try {
+      const productSale: QueryResult<ProductSale> = await pool.query(query, [
+        id,
+      ]);
+
+      if (productSale.rows.length === 0)
+        throw new ProductSaleError(
+          "product_sale_not_found",
+          "The product sale does not exist"
+        );
+
+      return productSale.rows[0] as ProductSale;
+    } catch (error) {
+      if (error instanceof ProductSaleError) throw error;
+
+      throw new DomainError(
+        "unknown",
+        `It was not possible to get the product sale: ${error}`
+      );
+    }
+  }
+
+  static async create(
+    productSale: Omit<ProductSale, "id">
+  ): Promise<ProductSale> {
+    const query = `
+        INSERT INTO product_sales (customer_id, item_id, quantity, value, date)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id, customer_id, item_id, quantity, value, date;
+    `;
+
+    try {
+      const createdProductSale: QueryResult<ProductSale> = await pool.query(
+        query,
+        [
+          productSale.customerId,
+          productSale.itemId,
+          productSale.quantity,
+          productSale.value,
+          productSale.date,
+        ]
+      );
+
+      return createdProductSale.rows[0] as ProductSale;
+    } catch (error) {
+      throw new DomainError(
+        "unknown",
+        `It was not possible to create the product sale: ${error}`
+      );
+    }
+  }
+
+  static async update(productSale: ProductSale): Promise<ProductSale> {
+    const query = `
+        UPDATE product_sales
+        SET customer_id = COALESCE($1, customer_id),
+            item_id = COALESCE($2, item_id),
+            quantity = COALESCE($3, quantity),
+            value = COALESCE($4, value),
+            date = COALESCE($5, date)
+        WHERE id = $6
+        RETURNING id, customer_id, item_id, quantity, value, date;
+    `;
+
+    try {
+      const updatedProductSale: QueryResult<ProductSale> = await pool.query(
+        query,
+        [
+          productSale.customerId,
+          productSale.itemId,
+          productSale.quantity,
+          productSale.value,
+          productSale.date,
+          productSale.id,
+        ]
+      );
+
+      if (updatedProductSale.rows.length === 0)
+        throw new ProductSaleError(
+          "product_sale_not_found",
+          "The product sale does not exist"
+        );
+
+      return updatedProductSale.rows[0] as ProductSale;
+    } catch (error) {
+      if (error instanceof ProductSaleError) throw error;
+
+      throw new DomainError(
+        "unknown",
+        `It was not possible to update the product sale: ${error}`
+      );
+    }
+  }
+
+  static async remove(id: number): Promise<ProductSale> {
+    const query = `
+          DELETE FROM product_sales
+          WHERE id = $1
+          RETURNING id, customer_id, item_id, quantity, value, date;
+      `;
+
+    try {
+      const removedProductSale: QueryResult<ProductSale> = await pool.query(
+        query,
+        [id]
+      );
+
+      if (removedProductSale.rows.length === 0)
+        throw new ProductSaleError(
+          "product_sale_not_found",
+          "The product sale does not exist"
+        );
+
+      return removedProductSale.rows[0] as ProductSale;
+    } catch (error) {
+      if (error instanceof ProductSaleError) throw error;
+
+      throw new DomainError(
+        "unknown",
+        `It was not possible to remove the product sale: ${error}`
+      );
+    }
+  }
+}

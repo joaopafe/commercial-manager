0a0f75ede73e134c6499615a43e1d4d59aeaa4fb
joaopafe <joao.ea2020@gmail.com>
2025-10-18 07:05:51 -0300

    add the customerRouter

---
+++ backend/src/presentation/routes/customerRouter.ts
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import { celebrate, Joi, Segments } from "celebrate";
+
+import { CustomerControllerFactory } from "../../providers/di/CustomerControllerFactory";
+
+export const customerRouter = Router();
+
+const customerControllerFactory = new CustomerControllerFactory();
+const customerController = customerControllerFactory.makeCustomerController();
+
+customerRouter.get("/", customerController.getAll);
+
+customerRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      cpf: Joi.string().min(11).required(),
+      name: Joi.string().min(2).required(),
+      email: Joi.string().email().required(),
+      phone: Joi.string().min(10).required(),
+    }),
+  }),
+  customerController.add
+);
+
+customerRouter.put(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().positive().required(),
+    }),
+  }),
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      cpf: Joi.string().min(11).required(),
+      name: Joi.string().min(2).required(),
+      email: Joi.string().email().required(),
+      phone: Joi.string().min(10).required(),
+    }),
+  }),
+  customerController.update
+);
+
+customerRouter.delete(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().positive().required(),
+    }),
+  }),
+  customerController.remove
+);
